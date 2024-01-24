d67e1acade0755f1f041eb8724dfa1daae8e6f7b
ezhou <ericzhou51@gmail.com>
2024-01-24 11:05:20 -0800

    henlo

--- core/src/com/bmhs/gdxintro/AppHandler.java
+++ core/src/com/bmhs/gdxintro/AppHandler.java
@@ -15,6 +15,7 @@ public class AppHandler extends ApplicationAdapter {
 	Texture img;
 
 	TextureRegion subImg;
+
 	int x, y;
 
 	int[][] Canvas = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
